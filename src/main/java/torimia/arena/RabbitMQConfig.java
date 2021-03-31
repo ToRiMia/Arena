7cc392ab65cf4d9e153f5abe93d3d709bae0cc2c
Viktoriia Omelchenko <omelchenko.viktoriia7@gmail.com>
2021-03-31 17:23:12 +0300

    Fixed RabbitMQ bugs

--- src/main/java/torimia/arena/RabbitMQConfig.java
+++ src/main/java/torimia/arena/RabbitMQConfig.java
@@ -2,6 +2,8 @@ package torimia.arena;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.AsyncRabbitTemplate;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
@@ -9,12 +11,23 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableRabbit
 public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue.battle.name}")
+    private String battleQueueName;
+
+    @Bean
+    Queue battleQueue() {
+        return QueueBuilder.durable(battleQueueName)
+                .autoDelete()
+                .build();
+    }
 
     @Bean
     public MessageConverter jsonMessageConverter() {
